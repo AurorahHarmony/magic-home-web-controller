d8fd7ba2a313abdc269f83201e2c7ed05fd1939f
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-06-18 21:33:17 +1200

    Connect to magic-home light and set to pink

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Server Settings
 const port = 8080;
 const staticDir = __dirname + '/static';
+const lightIp = '192.168.1.2';
 
 //Initialize Express Requirements
 const express = require('express');
@@ -11,6 +12,21 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
+//Initialize Magic-Home
+const {
+    Control
+} = require('magic-home');
+
+let light = new Control(lightIp);
+
+light.setColor(255, 10, 10).then(success => {
+    if (success) {
+        console.log('light is pink');
+    } else {
+        console.log('light is not pink');
+    }
+})
+//Server Routes
 app.get('/', (req, res) => {
     res.sendFile(staticDir + '/index.html');
 });
